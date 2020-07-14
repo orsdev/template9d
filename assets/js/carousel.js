61a7f4d5fd3f2a6ead300ed72d22187584c36172
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-07-14 09:17:01 +0100

    all bug fixed

---
+++ assets/js/carousel.js
@@ -0,0 +1,26 @@
+
+$('carousel-1').carousel({
+  interval: 2000
+});
+
+$('carousel-2').carousel({
+  interval: 1500
+})
+
+$('.carousel .carousel-item').each(function () {
+  var minPerSlide = 3;
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
